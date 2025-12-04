0d0bd03c95025dc9bc83f8d043d446d5074f7a0d
MinhThang <dominhtoan9467@gmail.com>
2025-12-04 21:39:11 +0700

    Sửa lại lỗi font

--- Program.cs
+++ Program.cs
@@ -6,23 +6,23 @@ namespace Menu_Management
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
-        // Khai bÃ¡o m?t logger cho Program.cs 
+        // Khai báo m?t logger cho Program.cs 
         [STAThread]
         static void Main()
         {
-             // Khá»i táº¡o Serilog
+            // Khởi tạo Serilog
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose() // Thiáº¿t láº­p má»©c log tá»i thiá»u lÃ  Verbose Äá» ghi láº¡i táº¥t cáº£ cÃ¡c má»©c log
-                .WriteTo.File("C:/Users/PC/OneDrive/Máy tính/Demo_GitFlow/logs", 
-                    rollingInterval: RollingInterval.Day, 
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")// Äá»nh dáº¡ng log vá»i timestamp, má»©c Äá» log, vÃ  thÃ´ng Äiá»p
+                .MinimumLevel.Verbose() // Thiết lập mức log tối thiểu là Verbose để ghi lại tất cả các mức log
+                .WriteTo.File("C:/Users/PC/OneDrive/Máy tính/Demo_GitFlow/logs/app_log.txt",
+                    rollingInterval: RollingInterval.Day,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")// Định dạng log với timestamp, mức độ log, và thông điệp
                 .CreateLogger();
-            Log.Information("----- á»¨NG Dá»¤NG KHá»I Äá»NG -----");
+            Log.Information("----- ỨNG DỤNG KHỞI ĐỘNG -----");
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new LoginForm());
-            // ÄÃ³ng log khi táº¯t app
+            // Đóng log khi tắt app
             Log.CloseAndFlush();
         }
     }
